857a06b75c2a6f1789892694974bc0edd006ed81
daly <dalyeu@yahoo.fr>
2018-09-19 01:12:19 -0400

    Add the country code and name json file and implemented the  display of the country name after the search by the user

--- src/js/main.js
+++ src/js/main.js
@@ -19,11 +19,18 @@ const mainTag = document.querySelector("main");
 const inputSearch = document.querySelector("#inputSearch");
 //Get the search form
 const searchForm = document.querySelector("form");
+//Get the current section
+const currentSection = document.querySelector(".current");
+//Get the city name div content ("h3")
+const cityAndCountry = document.querySelector("#cityNameAndCountryName");
 
 
 
 //Arrays of week days
 const weekDays = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+//Arrays of months
+const months = ["jan", "fev", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
 //Set a configuration Object
 let configuration = {
   lan: "en",
@@ -82,14 +89,30 @@ function saveSettings() {
   localStorage.setItem('config', JSON.stringify(parameters));
 
 }
+
 //Call the API
 function callTheApi(e) {
+  let displayTempUnit;
+  let displaySpeedUnit;
 
   let unit = JSON.parse(localStorage.getItem('config'))[0].temp;
   let lan = JSON.parse(localStorage.getItem('config'))[0].lan;
   let key = "6f82f2d2ceb2aa8ec59653c8cd278915";
   let nameOfCity = inputSearch.value.trim();
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${nameOfCity}&units=${unit}&lang=${lan}&APPID=${key}`;
+
+
+  // check the unit to display the good letter("F" or "C")
+  if (unit == "imperial") {
+    displayTempUnit = "F";
+    displaySpeedUnit = "mph";
+
+  }
+  if (unit == "metric") {
+    displayTempUnit = "C";
+    displaySpeedUnit = "m/s";
+  }
+
   // Check if the user entered something
   e.preventDefault();
   if (nameOfCity == "") {
@@ -99,16 +122,107 @@ function callTheApi(e) {
     .then(res => res.json())
     .then(data => {
       console.log(data);
+
+      let city = data.name;
+      let countryName = data.sys.country;
+      let groupId = data.weather[0].id;
+      let description = data.weather[0].description;
+      let tempNow = data.main.temp.toFixed();
+      let humidity = data.main.humidity.toFixed();
+      let minTemp = data.main.temp_min.toFixed();
+      let maxTemp = data.main.temp_max.toFixed();
+      let windSpeed = data.wind.speed.toFixed();
+      let date = new Date(data.dt * 1000);
+      let d = date.getDate();
+      let m = months[date.getMonth()];
+      let y = date.getFullYear();
+      let dateOnScreen = date.toLocaleDateString();
+      let sunRise = new Date(data.sys.sunrise * 1000).toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+      let sunSet = new Date(data.sys.sunset * 1000).toLocaleTimeString(navigator.language, {
+        hour: '2-digit',
+        minute: '2-digit'
+      });
+
+      // Generate the city name and country content
+      fetch("js/countriesCode.json")
+        .then(res => res.json())
+        .then(data => {
+          data.forEach(country => {
+
+            if (countryName == country.code) {
+              countryName = country.name.toUpperCase();
+            }
+          });
+          cityAndCountry.textContent = `
+        ${city}, ${countryName}
+        `;
+        });
+
+      //Generate the current content
+      currentSection.innerHTML = `
+            <div class="icon">
+                <h1><i class="wi wi-owm-${groupId}"></i></h1>
+                <h4>${description}</h4>
+            </div>
+            <div class="temperature">
+                <h1>${tempNow}&deg;</h1>
+                <h1>${displayTempUnit}</h1>
+                <h3>${dateOnScreen}</h3>
+            </div>
+            <div class="description">
+                <div class="description-title">
+                    <div class="sunRise">
+                        <i class="wi wi-horizon-alt"></i>
+                        <h6>${sunRise}</h6>
+                    </div>
+                    <div class="sunSet">
+                        <i class="wi wi-horizon"></i>
+                        <h6>${sunSet}</h6>
+                    </div>
+                </div>
+                <div class="wind">
+                    <div class="windSymbol">
+                        <h3><i class="wi wi-strong-wind"></i></h3>
+                    </div>
+                    <div class="windValue">
+                        <h5>${windSpeed}<span> ${displaySpeedUnit}</span></h5>
+                    </div>
+                </div>
+                <div class="minmaxtemp">
+                    <div class="thermometer">
+                        <h5><i class="wi wi-thermometer"></i></h5>
+                    </div>
+                    <div class="tempvalues">
+                        <div class="max">
+                            <h3>${maxTemp}&deg;</h3>
+                        </div>
+                        <div class="min">
+                            <h3>${minTemp}&deg;</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="humidity">
+                    <div class="humiditySymbol">
+                        <h4><i class="fas fa-tint"></i></h4>
+                    </div>
+                    <div class="humidityValue">
+                        <h4>${humidity}%</h4>
+                    </div>
+                </div>
+            </div>
+`;
+      searchDiv.classList.remove("show");
+      arrow.firstElementChild.innerHTML = `<i class="fas fa-angle-down arrow-down"></i>`;
+      cityName.firstElementChild.style.fontSize = "1.3rem";
+    })
+    .catch(() => {
+      cityAndCountry.textContent = `
+        City not found !
+        `;
+      alert("hint: Try somethimg like this: miami,us");
     });
 }
-//todo http://api.openweathermap.org/data/2.5/weather?q=
-//todo var date = new Date(1313564400000);
-
-//!var d = new Date();
-//!var n = d.getUTCDay();  from 0 to 6   0 =sunday
-
-//  function myFunction() {
-//  var d = new Date();
-//  var n = d.getUTCDate(); from 1 to 31
-//}
 searchForm.addEventListener("submit", callTheApi);
